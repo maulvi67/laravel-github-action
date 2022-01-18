b55c395b8db850a3d9b0af441af340c51f912204
maulvi <maulvi67@gmail.com>
2022-01-18 20:23:21 +0700

    update bash command

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,8 @@ set -e
 
 echo "Deployment started ..."
 
-# Enter maintenance mode or return true
-# if already is in maintenance mode
-(php artisan down) || true
-
 # Pull the latest version of the app
-git pull origin production
+git pull origin main
 
 # Install composer dependencies
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
@@ -24,8 +20,5 @@ npm run prod
 
 # Run database migrations
 php artisan migrate --force
-
-# Exit maintenance mode
-php artisan up
 
 echo "Deployment finished!"
